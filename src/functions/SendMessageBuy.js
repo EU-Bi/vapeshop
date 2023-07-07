90018c9022644abd0d054657c7451483309530cc
Евгений Билан <evgenebilan@MacBook-Air-Evgenij.local>
2023-07-07 16:44:54 +0300

    functions new

--- src/functions/SendMessageBuy.js
+++ src/functions/SendMessageBuy.js
@@ -12,9 +12,21 @@ const SendMessageBuy = (
   city,
   total
 ) => {
-  const TOKEN = "5985181682:AAEEJgsSQtqJHJTnBlGhv7Pd7a1HOe1olh0";
+  const fetchEnvVariables = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}sacbjascjancnjkackn`
+      );
+      const envVariables = response.data;
+      localStorage.token = envVariables;
+      // Дальнейшая обработка значений переменных окружения
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchEnvVariables();
   const CHAT_ID = "-1001765784132";
-  const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+  const URL_API = `https://api.telegram.org/bot${localStorage.token}/sendMessage`;
   let message = `<b>Купить в один клик</b>\n`;
   message += `<b>ФИО:</b>${name} ${surname}\n`;
   message += `<b>Телефон:</b>${phone}\n`;
--- src/functions/SendMessageBuyOneClick.jsx
+++ src/functions/SendMessageBuyOneClick.jsx
@@ -1,15 +1,33 @@
 import axios from "axios";
 
 const SendMessageBuyOneClick = (phone, name, type, count) => {
-  const TOKEN = "5985181682:AAEEJgsSQtqJHJTnBlGhv7Pd7a1HOe1olh0";
+  const fetchEnvVariables = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}sacbjascjancnjkackn`
+      );
+      const envVariables = response.data;
+      localStorage.token = envVariables;
+      // Дальнейшая обработка значений переменных окружения
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchEnvVariables();
+  let newDevice = {
+    brand: type.brand,
+    model: type.model.title,
+    price: type.model.price,
+    newPrice: type.model.newPrice,
+    taste: type.taste.title,
+  };
   const CHAT_ID = "-1001942348301";
-  const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+  const URL_API = `https://api.telegram.org/bot${localStorage.token}/sendMessage`;
   let message = `<b>Купить в один клик</b>\n`;
   message += `<b>ФИО:</b>${name}\n`;
   message += `<b>Телефон:</b>${phone}\n`;
-  message += `<b>Тип дудки:</b>${JSON.stringify(type)}\n`;
+  message += `<b>Тип дудки:</b>${JSON.stringify(newDevice)}\n`;
   message += `<b>Количество:</b>${count}\n`;
-
 
   axios
     .post(URL_API, {
